9a033abc239fba119aa943023e9780b5eb94c8ad
thomasgrastein <thomasgrastein@gmail.com>
2020-07-28 15:41:21 +0200

    fix

--- App.js
+++ App.js
@@ -1,43 +1,53 @@
 import React, { Component } from "react";
-import { BackHandler,StatusBar } from "react-native";
+import { View, Platform, TouchableOpacity } from "react-native";
 import { WebView } from "react-native-webview";
+import { StatusBar } from "expo-status-bar";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { Ionicons } from "@expo/vector-icons";
 
 export default class App extends Component {
   WEBVIEW_REF = React.createRef();
 
-  state = {
-    canGoBack: false,
-  };
-
-  componentDidMount() {
-    BackHandler.addEventListener("hardwareBackPress", this.handleBackButton);
-  }
-
-  componentWillUnmount() {
-    BackHandler.removeEventListener("hardwareBackPress", this.handleBackButton);
-  }
-
-  handleBackButton = () => {
-    if (this.state.canGoBack) {
-      this.WEBVIEW_REF.current.goBack();
-      return true;
-    }
-  };
-
-  onNavigationStateChange = (navState) => {
-    this.setState({
-      canGoBack: navState.canGoBack,
-    });
-  };
-
   render() {
-    return <>
-      <StatusBar hidden={false} />
-      <WebView
-        source={{ uri: "https://international.au.dk/life/studentscomingtoau/auapp/" }}
-        ref={this.WEBVIEW_REF}
-        onNavigationStateChange={this.onNavigationStateChange}
-        style={{ marginTop: 10 }} />
-     </>
+    return (
+      <SafeAreaView
+        style={{
+          flex: 1,
+          backgroundColor: "#002546",
+          justifyContent: "center",
+        }}
+      >
+        <StatusBar style="light" />
+        <WebView
+          source={{
+            uri: "https://international.au.dk/life/studentscomingtoau/auapp/",
+          }}
+          ref={this.WEBVIEW_REF}
+        />
+        <View
+          style={{
+            flex: 0.05,
+            flexDirection: "row",
+            justifyContent: "space-evenly",
+            paddingTop: 5,
+          }}
+        >
+          <TouchableOpacity onPress={() => this.WEBVIEW_REF.current.goBack()}>
+            <Ionicons
+              name={Platform.OS === "ios" ? "ios-arrow-back" : "md-arrow-back"}
+              size={32}
+              color="white"
+            />
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => this.WEBVIEW_REF.current.reload()}>
+            <Ionicons
+              name={Platform.OS === "ios" ? "ios-refresh" : "md-refresh"}
+              size={32}
+              color="white"
+            />
+          </TouchableOpacity>
+        </View>
+      </SafeAreaView>
+    );
   }
 }
